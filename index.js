5efc48f012173c5f3d4b64e806255ffe2f369247
MD Saifur Rahman <saifurrahmansaif954@gmail.com>
2026-08-16 23:11:05 +0600

    user delete api added

--- index.js
+++ index.js
@@ -175,6 +175,14 @@ async function run() {
                   res.send(result);
             });
 
+            // delete user data
+            app.delete("/users/:id", async (req, res) => {
+                  const id = req.params.id;
+                  const query = { _id: new ObjectId(id) };
+                  const result = await users_collection.deleteOne(query);
+                  res.send(result);
+            });
+
             // Send a ping to confirm a successful connection
             await client.db("admin").command({ ping: 1 });
             console.log("Successfully connected to MongoDB!");
